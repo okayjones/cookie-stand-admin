45d3e99de6bb8a249665ce1872904bd125acbcbb
Logan Jones <loganemilyjones@mac.com>
2021-04-10 18:48:41 -0700

    move out reporttable and style overview

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 
 import { useState } from 'react'
-import { hours } from '../assets/data'
 import Footer from '../components/footer'
 import Header from '../components/header'
+import ReportTable from '../components/report_table'
 
 export default function CookieStandAdmin() {
   const [cookieStands, setCookieStands] = useState([])
@@ -22,9 +22,9 @@ export default function CookieStandAdmin() {
   return (
     <div className="box-border min-h-screen bg-green-50">
       <Head>
-        <title>Cookie Stand Admin</title>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍪</text></svg>"/>
-        {/* from https://css-tricks.com/emojis-as-favicons/ */}
+            <title>Cookie Stand Admin</title>
+            <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍪</text></svg>"/>
+            {/* from https://css-tricks.com/emojis-as-favicons/ */}
       </Head>
       
       <Header title="Cookie Stand Admin"/>
@@ -63,48 +63,5 @@ export default function CookieStandAdmin() {
       </div>
     </form>
     )
-  }
-  
-  function ReportTable(props){
-    
-    if(cookieStands.length === 0){
-      return(
-        <div className="my-6 text-lg text-gray-700">No Cookie Stands Avaliable</div>
-      )
-    }
-
-    const total_row = hours.map((hour, index) => (
-      props.cookieStandsArray.reduce((sum, stand) => sum + stand.hourly_sales[index], 0))
-    )
-    
-    return(
-      <table className="w-2/3 mx-auto my-6 mb-14">
-        <thead>
-          <tr className="bg-green-500">
-            <th>Location</th>
-            {hours.map(hour => (<th>{hour}</th>))}
-            <th>Totals</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.cookieStandsArray.map(stand => (
-            <tr className="border border-gray-500 odd:bg-green-400 even:bg-green-300">
-              <td className="px-2 text-left border border-gray-500">{stand.location}</td>
-              {stand.hourly_sales.map(hour_total => (
-                <td className="border border-gray-500">{hour_total}</td>
-              ))}
-              <td>{stand.hourly_sales.reduce((a, b) => a + b, 0)}</td>
-            </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr className="bg-green-500">
-            <th className="border border-gray-500">Totals</th>
-            {total_row.map(total => (<th className="border border-gray-500">{total}</th>))} 
-            <th className="border border-gray-500">{total_row.reduce((a,b ) => a + b, 0)}</th>
-          </tr>
-        </tfoot>
-      </table>
-    ) 
   }
 }
--- pages/overview.js
+++ pages/overview.js
@@ -1,7 +1,37 @@
 import Head from 'next/head'
 import Link from 'next/Link'
+import Header from '../components/header'
+
 
 
 export default function Overview() {
-    return(<Link href="/"><a>Return to Main page</a></Link>)
+    return(
+        <div className="box-border min-h-screen bg-green-50">
+            <Head>
+                <title>Cookie Stand Admin</title>
+                <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍪</text></svg>"/>
+                {/* from https://css-tricks.com/emojis-as-favicons/ */}
+            </Head>
+            <Header title="Cookie Stand Admin"/>
+            <main className="h-full text-sm text-center">
+            <button className="px-4 py-2 mt-16 bg-blue-300 rounded">
+                <Link href="/">
+                <a className="flex items-center">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg" 
+                        className="w-6 h-6 mr-4" fill="none" 
+                        viewBox="0 0 24 24" 
+                        stroke="currentColor">
+                        <path 
+                            strokeLinecap="round" 
+                            strokeLinejoin="round" 
+                            strokeWidth={2} 
+                            d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
+                    </svg>
+                    Return to Main page
+                </a></Link>
+            </button>
+            </main>
+        </div>        
+    )
 }
