2b033a9c0f42f923bfee09b27bd0f2e5692b0d1a
Logan Jones <loganemilyjones@mac.com>
2021-04-13 14:47:43 -0700

    style update

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer(props) {
     return(
-        <footer className="p-3 mt-6 text-sm bg-green-500">
+        <footer className="p-4 mt-6 text-sm text-center bg-green-500">
             <p>{props.qty} Locations World Wide</p>
         </footer>
     )
