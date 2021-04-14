91757cdde6f7d661ba42c8c4c4145707cdebe7ad
Logan Jones <loganemilyjones@mac.com>
2021-04-14 11:32:01 -0700

    add login form, admin, header

---
+++ components/cookie-stand-header.js
@@ -0,0 +1,22 @@
+import Link from 'next/Link'
+
+export default function CookieStandHeader({ username, onLogout }){
+    return (
+      <header className="flex items-center justify-around p-3 text-2xl bg-green-500">
+        <h1 className="font-bold text-gray-800">Cookie Stand Admin</h1>
+        <div className="flex items-center justify-around gap-x-3">
+            <p className="px-2 text-sm bg-green-100 rounded-md">{username}</p>
+          <Link href="/">
+            <button className="px-2 text-sm text-white bg-green-700 rounded-md">
+              <a onClick={onLogout} >Sign Out</a>
+            </button>
+          </Link>
+          <Link href="/overview">
+            <button className="px-2 text-sm rounded-md bg-green-50">
+              <a>Overview</a>
+            </button>
+          </Link>
+        </div>
+      </header> 
+    )
+  }
