3989cea2cc149e42a03e569334525e576de9c481
Logan Jones <loganemilyjones@mac.com>
2021-04-14 16:35:20 -0700

    header, footer, new index page

--- components/footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer(props) {
-    return(
-        <footer className="p-4 mt-6 text-sm text-center bg-green-500">
-            <p>{props.qty} Locations World Wide</p>
-        </footer>
-    )
-}
