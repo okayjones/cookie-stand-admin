7001a6799c8c5df96828de50754f6c85bce1797b
Logan Jones <loganemilyjones@mac.com>
2021-04-13 17:03:26 -0700

    style update

--- components/header.js
+++ components/header.js
@@ -2,8 +2,8 @@ import Link from 'next/Link'
 
 export default function Header(props){
     return(
-      <header className="flex items-center justify-between p-3 text-2xl bg-green-500">
-        <h1 className="">{props.title}</h1>
+      <header className="flex items-center justify-between p-3 text-2xl bg-green-500 px-60">
+        <h1 className="font-bold">{props.title}</h1>
         <button className="px-1 text-sm rounded-sm bg-green-50">
         <Link href="/overview">
           <a>Overview</a>
