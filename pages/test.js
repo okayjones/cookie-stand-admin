3c8912726129aa6699780f04eddca1cee4191d80
Logan Jones <loganemilyjones@mac.com>
2021-04-14 11:31:03 -0700

    new index page as test

---
+++ pages/test.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { getToken } from '../services/data-fetcher'
+import CookieStandAdmin from '../components/cookie-stand-admin'
+import LoginForm from '../components/login-form'
+
+
+export default function Home() {
+
+    const [token, setToken] = useState();
+
+    const [username, setUsername] = useState('');
+
+    async function loginHandler(values) {
+
+        const fetchedToken = await getToken(values);
+
+        setToken(fetchedToken);
+
+        setUsername(values.username);
+    }
+
+    function logoutHandler() {
+        setToken(null);
+    }
+
+    if (!token) return <LoginForm onSubmit={ loginHandler }/>
+
+    return (
+        <CookieStandAdmin 
+            username={ username }
+            token={ token }
+            onLogout={ logoutHandler }
+        />
+    )
+}
+
+
