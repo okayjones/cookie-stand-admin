7166f14c2cadd4456d952c66dc3b4ff78e4a4955
Logan Jones <loganemilyjones@mac.com>
2021-04-14 08:00:14 -0700

    one more

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function CookieStandAdmin() {
       </Head>
       
       <Header title="Cookie Stand Admin"/>
-      <main className="h-full mx-auto text-sm text-center w-min lg:w-5/6">
+      <main className="w-5/6 h-full mx-auto text-sm text-center">
         <CreateForm />
         <ReportTable cookieStandsArray={cookieStands}/>
       </main>
