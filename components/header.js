06558fa16fcb5dd1c4f9037e5447bbb58d20cf37
Logan Jones <loganemilyjones@mac.com>
2021-04-13 17:16:04 -0700

    header spacing fix

--- components/header.js
+++ components/header.js
@@ -2,7 +2,7 @@ import Link from 'next/Link'
 
 export default function Header(props){
     return(
-      <header className="flex items-center justify-between p-3 text-2xl bg-green-500 px-60">
+      <header className="flex items-center p-3 text-2xl bg-green-500 justify-evenly">
         <h1 className="font-bold">{props.title}</h1>
         <button className="px-1 text-sm rounded-sm bg-green-50">
         <Link href="/overview">
