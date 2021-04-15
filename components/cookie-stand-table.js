5e34717fe4433d3b8f2cf17674f457883ace94c4
Logan Jones <loganemilyjones@mac.com>
2021-04-14 17:24:56 -0700

    table styling

--- components/cookie-stand-table.js
+++ components/cookie-stand-table.js
@@ -19,7 +19,7 @@ export default function CookieStandTable({ stands, onDelete }) {
                         <tr key={stand.id} className="text-right border border-green-500 odd:bg-green-200 even:bg-green-300">
                             <TH>
                                 <div>
-                                    <p className="float-left pl-4 pr-8">{stand.location}</p>
+                                    <p className="float-left pl-4">{stand.location}</p>
                                     <span className="float-right pr-2" onClick={() => onDelete(stand)}>
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
@@ -38,7 +38,7 @@ export default function CookieStandTable({ stands, onDelete }) {
                 })}
             </tbody>
             <tfoot>
-                <tr className="font-bold text-right bg-green-400">
+                <tr className="text-right bg-green-400">
                     <TH>Totals</TH>
                     {hours.map((_, i) => {
                         const amt = stands.reduce((acc, cur) => acc + cur.cookiesEachHour[i], 0);
@@ -54,7 +54,7 @@ export default function CookieStandTable({ stands, onDelete }) {
 
 function Table({ children }) {
     return (
-        <table className="m-2 mx-auto text-sm border border-green-500">
+        <table className="w-full m-2 mx-auto text-sm border border-green-500">
             {children}
         </table>
     );
@@ -67,6 +67,6 @@ function TH({ children }) {
 
 function TD({ children }) {
     return (
-        <td className="p-1 pl-6 border border-green-500">{children}</td>
+        <td className="p-1.5 border border-green-500">{children}</td>
     )
 }
