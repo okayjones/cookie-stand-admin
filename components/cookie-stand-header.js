7e176bc02a7581922b3caa7f749d28b844e3e30f
Logan Jones <loganemilyjones@mac.com>
2021-04-14 18:27:16 -0700

    hide sign out if signed out

--- components/cookie-stand-header.js
+++ components/cookie-stand-header.js
@@ -7,9 +7,9 @@ export default function CookieStandHeader({ username, onLogout }){
         <div className="flex items-center justify-around gap-x-3">
             <p className="px-2 text-sm bg-green-100 rounded-md">{username}</p>
           <Link href="/">
-            <button className="px-2 text-sm text-white bg-green-700 rounded-md">
-              <a onClick={onLogout} >Sign Out</a>
-            </button>
+                <button className="px-2 text-sm text-white bg-green-700 rounded-md">
+                  { username  &&  <a onClick={onLogout} >Sign Out</a> }
+                </button>
           </Link>
           <Link href="/overview">
             <button className="px-2 text-sm rounded-md bg-green-50">
